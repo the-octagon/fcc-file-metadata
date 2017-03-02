1e87683dd9a688153216123af8353f9801428143
Andrew King <andrew.f.king@gmail.com>
2017-03-02 17:51:36 +0000

    delete tmp files after run

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var multer = require('multer');
 var upload = multer({ dest: './tmp/'});
+var fs = require('fs');
 
 
 app.get('/', function (req, res) {
@@ -9,9 +10,9 @@ app.get('/', function (req, res) {
 });
 
 app.post('/get-file-size', upload.single('userFile'), function (req, res) {
-    
-    res.send({size: req.file.size});
-    //need to delete tmp files
+    var response = {size: req.file.size}
+    res.send(response);
+    fs.unlinkSync(req.file.path);
 });
 
 app.listen(8080, function () {
