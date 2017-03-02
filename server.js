bcc857de9a13ea90be45170d7408f0124d86d102
Andrew King <andrew.f.king@gmail.com>
2017-03-02 15:59:07 +0000

    kinda working

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var multer = require('multer');
+var upload = multer({ dest: './tmp/'});
+
+
+app.get('/', function (req, res) {
+    res.send('Submit a file to view its filesize:<br><br><form method="post" action="/get-file-size" enctype="multipart/form-data"><input type="file" name="userFile" accept="*"><input type="submit"></form> ');
+});
+
+app.post('/get-file-size', upload.single('userFile'), function (req, res) {
+    
+    res.send({size: req.file.size});
+});
+
+app.listen(8080, function () {
+  console.log('file metadata server is running');
+})
